053e702fbf0ccf82567d92db3d971bf0c01f2830
Pratik <pratik.vernekar@robosoftin.com>
2023-02-01 12:44:39 +0530

    signIN page done

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
-import { useFormik } from "formik";
+import { Formik, useFormik } from "formik";
 import eyeOn from "../../images/eye(1).png";
 import eyeoff from "../../images/eyeoff.png";
 import "./SignIn.css";
+import * as Yup from "yup";
 
+const SignInSchema = Yup.object().shape({
+  mobile: Yup.string()
+    .min(10, "Too Short!")
+    .max(10, "Too Long!")
+    .required("Required"),
+  mpin: Yup.string()
+    .min(4, "Too Short!")
+    .max(4, "Too Long!")
+    .required("Required"),
+});
 function SignIn() {
-  const [textSecure, setTextSecure] = useState(false);
+  const [textSecure, setTextSecure] = useState(true);
   const formik = useFormik({
     initialValues: {
       mobile: "",
       mpin: "",
     },
+    validationSchema: SignInSchema,
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
     },
@@ -19,6 +31,8 @@ function SignIn() {
     <div className="signin-container">
       <form onSubmit={formik.handleSubmit} className="form-container">
         <input
+         autoComplete="off"
+          maxLength={10}
           className="input"
           placeholder="Mobile Number"
           id="mobile"
@@ -27,6 +41,11 @@ function SignIn() {
           onChange={formik.handleChange}
           value={formik.values.mobile}
         />
+
+        {formik.errors.mobile && formik.touched.mobile ? (
+          <p className="form-error1"> {formik.errors.mobile}</p>
+        ) : null}
+
         <img
           src={textSecure ? eyeOn : eyeoff}
           onClick={() => {
@@ -35,6 +54,8 @@ function SignIn() {
           className="eye"
         />
         <input
+         autoComplete="off"
+          maxLength={4}
           className="input"
           placeholder="MPin"
           id="mpin"
@@ -43,7 +64,12 @@ function SignIn() {
           onChange={formik.handleChange}
           value={formik.values.mpin}
         />
-        <p>Forgot your password?</p>
+
+        {formik.errors.mpin && formik.touched.mobile ? (
+          <p className="form-error2"> {formik.errors.mpin} </p>
+        ) : null}
+
+        <p className="forget-text">Forgot your password?</p>
         <button type="submit" className="submit-btn">
           SIGN IN
         </button>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormik } from "formik";
+import eyeOn from "../../images/eye(1).png";
+import eyeoff from "../../images/eyeoff.png";
+import "./SignUp.css";
+import * as Yup from "yup";
+const SignUpSchema = Yup.object().shape({
+  mobile: Yup.string()
+    .min(10, "Too Short!")
+    .max(10, "Too Long!")
+    .required("Required"),
+  mpin: Yup.string()
+    .min(4, "Too Short!")
+    .max(4, "Too Long!")
+    .required("Required"),
+  mpinconfirm: Yup.string().oneOf([Yup.ref("mpin"), null], "MPin must match"),
+});
 
 function SignUp() {
-  return <div>SignUp</div>;
+  const [textSecure, setTextSecure] = useState(true);
+  const formik = useFormik({
+    initialValues: {
+      mobile: "",
+      mpin: "",
+      mpinconfirm: "",
+    },
+    validationSchema: SignUpSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+  return (
+    <div className="signin-container">
+      <form onSubmit={formik.handleSubmit} className="form-container">
+        <input
+          autoComplete="off"
+          maxLength={10}
+          className="input"
+          placeholder="Mobile Number"
+          id="mobile"
+          name="mobile"
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.mobile}
+        />
+        {formik.errors.mobile && formik.touched.mobile ? (
+          <p className="form-error1"> {formik.errors.mobile}</p>
+        ) : null}
+        <img
+          src={textSecure ? eyeOn : eyeoff}
+          onClick={() => {
+            setTextSecure(!textSecure);
+          }}
+          className="eye-1"
+        />
+        <input
+          autoComplete="off"
+          maxLength={4}
+          className="input"
+          placeholder="Enter 4 digit MPin"
+          id="mpin"
+          name="mpin"
+          type={textSecure ? "password" : "text"}
+          onChange={formik.handleChange}
+          value={formik.values.mpin}
+        />
+        <input
+          autoComplete="off"
+          maxLength={4}
+          className="input"
+          placeholder="Re-Enter 4 digit MPin"
+          id="mpinconfirm"
+          name="mpinconfirm"
+          type={textSecure ? "password" : "text"}
+          onChange={formik.handleChange}
+          value={formik.values.mpinconfirm}
+        />
+        {formik.errors.mpin && formik.touched.mpin ? (
+          <p className="form-error2"> {formik.errors.mpin}</p>
+        ) : null}
+        {formik.errors.mpinconfirm && formik.touched.mpinconfirm ? (
+          <p className="form-error3"> {formik.errors.mpinconfirm}</p>
+        ) : null}
+
+        <button type="submit" className="submit-btn">
+          SIGN IN
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUp;
