763611eed530ec2dd30c0d80f36e127eba314cee
Pratik <pratik.vernekar@robosoftin.com>
2023-02-02 09:57:47 +0530

    pass manager page done

--- src/pages/PassManager/PassManager.js
+++ src/pages/PassManager/PassManager.js
@@ -5,10 +5,12 @@ import passManager from "../../images/06/Group/passmanager.png";
 import profileImg from "../../images/06/Group/profile.png";
 import searchImg from "../../images/06/Group/search.png";
 import syncImg from "../../images/06/Group/syncicn.png";
+import pathIcon from "../../images/07/Group7/Path.png";
+import addbtn from "../../images/addbtn.png";
 
 function PassManager() {
   return (
-    <div>
+    <div className="passManager-main">
       <div className="header">
         <div className="header-1">
           <img className="header-img" src={barIcon}></img>
@@ -16,11 +18,21 @@ function PassManager() {
         </div>
 
         <div className="header-2">
-          <img  className="header-img3"src={searchImg}></img>
-          <img  className="header-img3"src={syncImg}></img>
-          <img  className="header-img3"src={profileImg}></img>
+          <img className="header-img3" src={searchImg}></img>
+          <img className="header-img3" src={syncImg}></img>
+          <img className="header-img3" src={profileImg}></img>
         </div>
       </div>
+      <div className="sub-header">
+        <p className="sites-text">Sites</p>
+        <div className="sites-border"></div>
+        <div className="sub-header-1">
+          Social Media
+          <div className="circle">07</div>
+          <img className="path-img" src={pathIcon}></img>
+        </div>
+      </div>
+      <img className="addSite-Btn" src={addbtn}></img>
     </div>
   );
 }
